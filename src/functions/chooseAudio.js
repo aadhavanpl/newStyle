22a734d056f6595a1902d4acc12bb7617616fc53
aadhavanpl <aadhavanlenin@gmail.com>
2022-06-17 12:27:43 +0300

    changed audio file format and added new audio files

--- src/functions/chooseAudio.js
+++ src/functions/chooseAudio.js
@@ -1,10 +1,9 @@
 import { finalAns } from "../components/buttons/Buttons"
-import "../audio/ஆல்.m4a"
 
 function chooseAudio() {
     var source = document.getElementById("source");
     var audio = document.getElementById("idAudio"); 
-    source.src =  "http://127.0.0.1:8080/" + finalAns.value + ".m4a"
+    source.src =  "http://127.0.0.1:8080/" + finalAns.value + ".mp3"
     console.log(source.src)
     audio.load();
 }
