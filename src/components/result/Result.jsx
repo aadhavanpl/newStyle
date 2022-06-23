0aafa09d81c2826826f6a20c107dcb86004e1610
aadhavanpl <aadhavanlenin@gmail.com>
2022-06-23 19:53:31 +0300

    msc changes

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Result = () => {
     return (
         <section className=''>
-            <div className='userAns'>
-            </div>
+            <div className='userAns'></div>
         </section>
     )
 }
--- src/functions/check.js
+++ src/functions/check.js
@@ -5,10 +5,13 @@ function check(){
     if (usersAns.value === finalAns.value) {
         console.log("Correct")
         usersAns.value = ""
+        document.getElementById("ans").innerHTML = usersAns.value
+        document.getElementById("check").innerHTML = "Correct"
         newWord()
     }
     else {
         console.log("Incorrect")
+        document.getElementById("check").innerHTML = "Incorrect"
         usersAns.value = ""
     }
 }
