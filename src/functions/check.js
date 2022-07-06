f56eaa2187dd9bb850343b365ee4e2ad5b24303a
AadhavanPL <aadhavanlenin@gmail.com>
2022-07-06 21:51:35 +0300

    added timeout function and refined choose audio

--- src/functions/check.js
+++ src/functions/check.js
@@ -5,14 +5,21 @@ function check(){
     if (usersAns.value === finalAns.value) {
         console.log("Correct")
         usersAns.value = ""
-        document.getElementById("ans").innerHTML = usersAns.value
         document.getElementById("check").innerHTML = "Correct"
         newWord()
+        setTimeout(function(){
+            document.getElementById("ans").innerHTML = usersAns.value
+            document.getElementById("check").innerHTML = "";
+        }, 2000);
     }
     else {
         console.log("Incorrect")
-        document.getElementById("check").innerHTML = "Incorrect"
         usersAns.value = ""
+        document.getElementById("check").innerHTML = "Incorrect"
+        setTimeout(function(){
+            document.getElementById("ans").innerHTML = usersAns.value
+            document.getElementById("check").innerHTML = "";
+        }, 2000);
     }
 }
 export default check
--- src/functions/chooseAudio.js
+++ src/functions/chooseAudio.js
@@ -3,7 +3,7 @@ import { finalAns } from "../components/buttons/Buttons"
 function chooseAudio() {
     var source = document.getElementById("source");
     var audio = document.getElementById("idAudio"); 
-    source.src =  "http://localhost/audio/" + finalAns.value + ".mp3"
+    source.src =  "http://37.208.160.151:25585/dashboard/" + finalAns.value + ".mp3"
     console.log(source.src)
     audio.load();
 }
